9978d092df855868d163fe5c83e26b254a771634
Rodeni4 <rodenkosv@gmail.com>
2020-12-01 12:57:50 +0700

    Push via IDE module_4 add code style

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -1,11 +1,8 @@
-import time
-
-
+# Data
 link_page = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 button_add_to_basket_locator = ".btn-add-to-basket"
 language_locator = "[selected='selected']"
-
 
 button_text_ru = "Добавить в корзину"
 button_text_en_GB = "Add to basket"
@@ -14,13 +11,18 @@ button_text_fr = "Ajouter au panier"
 
 
 def test_add_to_basket_button(browser):
+
+    # Arrange
     browser.get(link_page)
+
+    # Act
     button_add_to_basket = browser.find_element_by_css_selector(button_add_to_basket_locator)
     successful_button_text = button_add_to_basket.text
 
     language = browser.find_element_by_css_selector(language_locator)
     language_text = language.text
 
+    # Assert
     if "Русский" in language_text:
         assert button_text_ru in successful_button_text, "button text is different"
     elif "British English" in language_text:

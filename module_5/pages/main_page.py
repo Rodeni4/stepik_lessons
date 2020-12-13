4e34504c9fd15d591b8c80bf5f91d183a1797c14
Rodeni4 <rodenkosv@gmail.com>
2020-12-13 18:35:30 +0700

    No errors

--- module_5/pages/main_page.py
+++ module_5/pages/main_page.py
@@ -4,8 +4,8 @@ from .base_page import BasePage
 class MainPage(BasePage):
     link = "http://selenium1py.pythonanywhere.com/"
 
-    def __init__(self, *args, **kwargs):
-        super(MainPage, self).__init__(*args, **kwargs)
+    def __init__(self, browser):
+        super(MainPage, self).__init__(browser, self.link)
 
 
 

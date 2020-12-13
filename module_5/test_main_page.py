62d690292264bdf692a985ed25b804647cc738ac
Rodeni4 <rodenkosv@gmail.com>
2020-12-13 14:50:50 +0700

    My implementation of LoginPage and transitions between pages

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
@@ -12,3 +13,26 @@ class TestMainPage:
         page = MainPage(browser)
         page.open()
         page.should_be_login_link()
+
+
+class TestLoginPage:
+    def test_quest_should_be_login_url(self, browser):
+        page = MainPage(browser)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_url()
+
+    def test_quest_should_be_login_form(self, browser):
+        page = MainPage(browser)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_form()
+
+    def test_quest_should_be_register_form(self, browser):
+        page = MainPage(browser)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_register_form()
